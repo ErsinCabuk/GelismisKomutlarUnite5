d9ba13eb66ff6e09320186136f6697e6dcc68ee2
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2023-01-13 13:09:28 +0300

    13.01.2023 - Uygulama 5 yapıldı

---
+++ app/src/main/java/ec/ersincabuk/gelismiskomutlarunite5/ElektrikliCihaz.java
@@ -0,0 +1,23 @@
+package ec.ersincabuk.gelismiskomutlarunite5;
+
+import android.util.Log;
+
+public class ElektrikliCihaz {
+    public String deviceName;
+    public double devicePower;
+    public double deviceDaily;
+
+    //Kilowatt yerine Watt'a çevirdim
+    public double monthlyUsage() {
+        double monthlyUsagedPower = (devicePower / 1000) * deviceDaily * 30;
+        return monthlyUsagedPower;
+    }
+
+    public double price() {
+        double doubleDevicePower = monthlyUsage();
+        double result = 0;
+        if(doubleDevicePower > 150) result = (150 * 1.37) + ((doubleDevicePower - 150) * 2.06);
+        else result = doubleDevicePower * 1.37;
+        return result;
+    }
+}
--- app/src/main/java/ec/ersincabuk/gelismiskomutlarunite5/Uyg5Activity.java
+++ app/src/main/java/ec/ersincabuk/gelismiskomutlarunite5/Uyg5Activity.java
@@ -3,17 +3,45 @@ package ec.ersincabuk.gelismiskomutlarunite5;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.gelismiskomutlarunite5.R;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 public class Uyg5Activity extends AppCompatActivity {
+    EditText textDeviceName, textDevicePower, textDeviceDaily;
+    TextView textResult;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.uyg5_activity);
+
+        textDeviceName = findViewById(R.id.textDeviceName);
+        textDevicePower = findViewById(R.id.textDevicePower);
+        textDeviceDaily = findViewById(R.id.textDeviceDaily);
+        textResult = findViewById(R.id.textResult);
+    }
+
+    public void btnSuccess(View view) {
+        String deviceName = textDeviceName.getText().toString();
+        double kw = Double.parseDouble(textDevicePower.getText().toString());
+        int daily = Integer.parseInt(textDeviceDaily.getText().toString());
+
+        ElektrikliCihaz device = new ElektrikliCihaz();
+        device.deviceName = deviceName;
+        device.devicePower = kw;
+        device.deviceDaily = daily;
+        NumberFormat formatter = new DecimalFormat("#0.00");
+        String resultMonthly = formatter.format(device.monthlyUsage());
+        String resultPrice = formatter.format(device.price());
+        textResult.setText("Aylık Kullanılan KW: " + resultMonthly + " KW \nFatura: " + resultPrice + " TL");
     }
 
     public void btnBack(View view) {
