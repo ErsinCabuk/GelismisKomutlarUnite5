850a9f8ba0f9264f8408e9a28c6e1832ef6729e1
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2023-03-10 09:38:08 +0300

    10.03.2023 - Uygulama 12 yapıldı

--- app/src/main/java/ec/ersincabuk/gelismiskomutlarunite5/Uyg12Activity.java
+++ app/src/main/java/ec/ersincabuk/gelismiskomutlarunite5/Uyg12Activity.java
@@ -3,17 +3,44 @@ package ec.ersincabuk.gelismiskomutlarunite5;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.gelismiskomutlarunite5.R;
 
+import java.util.ArrayList;
+
 public class Uyg12Activity extends AppCompatActivity {
+    EditText txtIsim;
+    ListView listIsimler;
+    ArrayList<String> isimlerListesi = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.uyg12_activity);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, isimlerListesi);
+        txtIsim = findViewById(R.id.txtIsim);
+        listIsimler = findViewById(R.id.listIsimler);
+        listIsimler.setAdapter(adapter);
+
+        listIsimler.setOnItemClickListener((adapterView, view, i, l) -> {
+            isimlerListesi.remove(i);
+            adapter.notifyDataSetChanged();
+        });
+    }
+
+    public void btnEkle(View view) {
+        String ad = txtIsim.getText().toString();
+        isimlerListesi.add(ad);
+        adapter.notifyDataSetChanged();
+        txtIsim.getText().clear();
     }
 
     public void btnBack(View view) {
